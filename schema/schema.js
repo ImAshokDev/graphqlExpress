35ae0a721e8c62f11098eedeab4dcbb7a79487bf
krist yohan <massashok420@gmail.com>
2020-04-09 10:11:33 +0530

    list of books | list of author

--- schema/schema.js
+++ schema/schema.js
@@ -75,6 +75,18 @@ const RootQuery = new GraphQLObjectType({
         return _.find(author, {id: args.id});
       },
     },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      },
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors;
+      },
+    },
   },
 });
 
