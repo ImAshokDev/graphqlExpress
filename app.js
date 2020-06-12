2de1e97526dee012ccb35f5a11f2501759c4f19d
krist yohan <massashok420@gmail.com>
2020-06-12 12:41:56 +0530

    spacex api added

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema');
+// const express = require('express');
+// const graphqlHTTP = require('express-graphql');
+// const schema = require('./schema/schema');
+
+// const app = express();
+
+// app.use(
+//   '/graphql',
+//   graphqlHTTP({
+//     schema,
+//     graphiql: true,
+//   })
+// );
+
+// app.listen(4000, () => {
+//   console.log('Server running on port 4000');
+// });
+
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const SpaceXschema = require("./schema/SpaceXSchema");
 
 const app = express();
 
 app.use(
-  '/graphql',
+  "/graphql",
   graphqlHTTP({
-    schema,
+    schema: SpaceXschema,
     graphiql: true,
   })
 );
 
 app.listen(4000, () => {
-  console.log('Server running on port 4000');
+  console.log("server running on  port 4000");
 });
--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,5 @@
-const graphql = require('graphql');
-const _ = require('lodash');
+const graphql = require("graphql");
+const _ = require("lodash");
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -11,68 +11,68 @@ const {
 
 // dummy data
 var books = [
-  {name: 'book 1', genre: 'genre 1', id: '1', authorId: '1'},
-  {name: 'book 2', genre: 'genre 2', id: '2', authorId: '2'},
-  {name: 'book 3', genre: 'genre 3', id: '3', authorId: '3'},
-  {name: 'book 4', genre: 'genre 4', id: '4', authorId: '2'},
-  {name: 'book 6', genre: 'genre 5', id: '5', authorId: '3'},
-  {name: 'book 7', genre: 'genre 6', id: '6', authorId: '3'},
+  { name: "book 1", genre: "genre 1", id: "1", authorId: "1" },
+  { name: "book 2", genre: "genre 2", id: "2", authorId: "2" },
+  { name: "book 3", genre: "genre 3", id: "3", authorId: "3" },
+  { name: "book 4", genre: "genre 4", id: "4", authorId: "2" },
+  { name: "book 6", genre: "genre 5", id: "5", authorId: "3" },
+  { name: "book 7", genre: "genre 6", id: "6", authorId: "3" },
 ];
 
 var author = [
-  {name: 'ashok', age: '21', id: '1'},
-  {name: 'khrist', age: '22', id: '2'},
-  {name: 'yohan', age: '24', id: '3'},
+  { name: "ashok", age: "21", id: "1" },
+  { name: "khrist", age: "22", id: "2" },
+  { name: "yohan", age: "24", id: "3" },
 ];
 
 const BookType = new GraphQLObjectType({
-  name: 'Book',
+  name: "Book",
   fields: () => ({
-    id: {type: GraphQLID},
-    name: {type: GraphQLString},
-    genre: {type: GraphQLString},
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
     author: {
       type: AuthorType,
       resolve(parent, args) {
-        return _.find(author, {id: parent.authorId});
+        return _.find(author, { id: parent.authorId });
       },
     },
   }),
 });
 
 const AuthorType = new GraphQLObjectType({
-  name: 'Author',
+  name: "Author",
   fields: () => ({
-    id: {type: GraphQLID},
-    name: {type: GraphQLString},
-    age: {type: GraphQLInt},
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return _.filter(books, {authorId: parent.id});
+        return _.filter(books, { authorId: parent.id });
       },
     },
   }),
 });
 
 const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
+  name: "RootQueryType",
   fields: {
     book: {
       type: BookType,
-      args: {id: {type: GraphQLID}},
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data from db/other source
         console.log(typeof args.id);
-        return _.find(books, {id: args.authorId});
+        return _.find(books, { id: args.authorId });
       },
     },
     author: {
       type: AuthorType,
-      args: {id: {type: GraphQLID}},
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data from db/other source
-        return _.find(author, {id: args.id});
+        return _.find(author, { id: args.id });
       },
     },
     books: {
